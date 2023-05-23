193ce628ad3b81a86096260237223e8e6cdd0cf9
Marina Crespo <marinacrespog@gmail.com>
2023-05-22 21:03:25 -0300

    general changes

---
+++ my-app/src/components/banner/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>Do you have a big list of teeeeeeedious shit to do? Use dopamine driven tasks to plan and reward yourself</div>
+  )
+}
+
+export default Banner
